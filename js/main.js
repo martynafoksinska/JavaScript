ebd45bab8fd71a8b162c6d991172b157bb91c4aa
Martyna Foksinska <martyna.a.foksinska@gmail.com>
2022-08-30 11:08:44 +0200

    Działania arytmetyczne JS

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const add = 2 + 3
+console.log(`Wynik dodawania to: ${add}`)
+
+const substract = 10 - 5
+console.log(`Wynik odejmowania: ${substract}`)
+
+const multiply = 2 * 5
+console.log(`Wynik mnożenia: ${multiply}`)
+
+const divide = 10 - 2
+console.log(`Wynik dzielenia: ${divide}`)
+
+const modulo = 13 % 2
+console.log(`Wynik modulo: ${modulo}`)
+
+const name = 'Martyna'
+let age
+const food = 'hamburger'
+
+age = 26
+
+
+console.log(`Cześć, mam na imię ${name}, mam ${age} lat i moje ulubione jedzienie to ${food}.`)
