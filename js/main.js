74c7498d0848f839eac7d4471d6f42cea9a518d8
Martyna Foksinska <martyna.a.foksinska@gmail.com>
2022-08-30 15:12:42 +0200

    addEventListener

--- js/main.js
+++ js/main.js
@@ -105,3 +105,19 @@ if (numbers.length > 5) {
 } else {
 	console.log('FALSE')
 }
+
+// Pobieranie elementów
+
+const btn1 = document.querySelector('#btn1')
+const btn2 = document.querySelector('.btn2')
+
+function handleClick () {
+	console.log("Kliknięto przycisk nr 1");
+}
+
+function addd() {
+	console.log(2 + 2);
+}
+
+btn1.addEventListener('click', handleClick)
+btn2.addEventListener('click', addd)
