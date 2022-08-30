a9976b77eca7a6c83355905fb9093700accab27e
Martyna Foksinska <martyna.a.foksinska@gmail.com>
2022-08-30 12:54:36 +0200

    warunek if hasło

--- js/main.js
+++ js/main.js
@@ -19,5 +19,39 @@ const food = 'hamburger'
 
 age = 26
 
-
 console.log(`Cześć, mam na imię ${name}, mam ${age} lat i moje ulubione jedzienie to ${food}.`)
+
+const score = 15
+
+if (score % 2 == 0) {
+	console.log('Liczba parzysta')
+} else {
+	console.log('Liczba nieparzysta')
+}
+
+let num1 = 8
+
+num1++
+num1++
+
+console.log(num1)
+
+let x = 5
+const y = 10
+
+console.log((x *= y))
+console.log((x /= y))
+
+// Bardzo dobre hasło ma więcej niż 10 znaków
+// Dobre hasło ma 5-10 znaków
+// Słabe hasło ma mniej niż 5 znaków
+
+const passLength = 15
+
+if (passLength > 10) {
+	console.log(`Masz bardzo dobre hasło!`)
+} else if (passLength >= 5 && passLength <= 10) {
+	console.log(`Masz dobre hasło`)
+} else {
+	console.log(`Masz słabe hasło...`)
+}
