b72daf5df0bad7e4f3124142d949a76bc23929ef
martynafoksinska <martyna.a.foksinska@gmail.com>
2022-09-28 08:34:10 +0200

    Files added

---
+++ Kurs JavaScript/JS/main.js
@@ -0,0 +1,7 @@
+const name = "Martyna"
+let age = 26
+let favouriteDish = "rosół"
+
+console.log("Cześć, jestem " + name + " mam " + age + " lat i moim ulubionym daniem jest " + favouriteDish + ".");
+
+document.write("Cześć, jestem " + name + " mam " + age + " lat i moim ulubionym daniem jest " + favouriteDish + ".");
